7ef05fadb430dfdf38f517f1cb502e0271d752aa
Keris111 <136007895+Keris111@users.noreply.github.com>
2023-06-21 13:48:11 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,66 @@
-from openAiAPI import *
-from saveData import save_current_mood,  save_current_input, load_previous_input
-from googleAPI import YouTubeAPI
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.widget import Widget
+from kivy.animation import Animation
+from kivy.core.window import Window
 
-# Main interaction loop
-def main():
-    api_key_you_tube = 'AIzaSyDqMERs1kaVME3xDDjg9Zd9S67Tf12yjdw'
 
-    premium = False
+class SlideBar(BoxLayout):
+    def __init__(self, **kwargs):
+        super(SlideBar, self).__init__(**kwargs)
+        self.orientation = 'vertical'
 
-    def premium_client(): #We should combine the previous input with the client_mood of the client as well
-        if premium:
-            previous_input = load_previous_input()  # We should create such response only if premium also if premium is true we should make the response faster and more efficient
-            user_input = input("User input: ")
+        self.slide_line = Widget(size_hint=(1, 0.03))
+        self.add_widget(self.slide_line)
 
-            if previous_input:
-                user_input = previous_input + '\n' + user_input
-        else:
-             user_input = input("User input: ")
-        return user_input
+        self.label = Label(text='Slide up', size_hint=(1, 0.07))
+        self.add_widget(self.label)
 
-    user_input = premium_client() #Here is a short version if the client is not on premium we will just ask him (we should create a function which will be called if the client is not premium)
+        self.content = RelativeLayout(size_hint=(1, 0))
+        self.search_bar = TextInput(
+            size_hint=(0.8, None),
+            height=40,
+            pos_hint={'center_x': 0.5, 'center_y': 0.5}
+        )
+        self.content.add_widget(self.search_bar)
+        self.add_widget(self.content)
 
-    response = generate_response(user_input)
-    print("AI: " + response) #The response from AI
-    save_current_input(user_input)
+        self.slide_animation = Animation(height=Window.height * 0.9, duration=0.3)
+        self.slide_animation.bind(on_complete=self.on_slide_complete)
+        self.slide_down_animation = Animation(height=0, duration=0.3)
+        self.slide_down_animation.bind(on_complete=self.on_slide_down_complete)
 
-    def determine_mood(): #We are tellling the AI to take choose the client_mood our client has depending on the client request
-        determine_client_mood = "'" + user_input + "'" + " , based on my  request, what client_mood am I: 'sad', 'happy', 'fear', 'angry', 'neutral' (One word, output) "
-        client_mood = generate_response(str(determine_client_mood)) #In the data of AI he knows the previous request so we determine the client_mood
-        return client_mood
+        self.slide_up = False
 
-    mood = determine_mood() #Determening the client_mood
-    print("AI decision: " + mood)
-    save_current_mood(mood)
+        self.slide_line.size_hint = (1, 0.02)  # Initial shorter line
 
-    #googleAPI = YouTubeAPI(api_key_you_tube)
-    #googleAPI.search_for_video(user_input)
+    def on_touch_down(self, touch):
+        if touch.pos[1] < self.height * 0.1 and not self.slide_up:
+            self.slide_up = True
+            self.slide_animation.start(self.content)
+            self.slide_line.size_hint = (1, 0.04)  # Longer line after sliding
+            return True
+        return super(SlideBar, self).on_touch_down(touch)
 
-# Run the program
-if __name__ == "__main__":
-    main()
+    def on_slide_complete(self, animation, widget):
+        self.search_bar.focus = True
+
+    def on_slide_down_complete(self, animation, widget):
+        self.search_bar.focus = False
+        self.slide_up = False
+        self.slide_line.size_hint = (1, 0.03)  # Reset line size
+
+
+class SlideBarApp(App):
+    def build(self):
+        layout = BoxLayout(orientation='vertical')
+        slide_bar = SlideBar()
+        layout.add_widget(slide_bar)
+        return layout
+
+
+if __name__ == '__main__':
+    SlideBarApp().run()
